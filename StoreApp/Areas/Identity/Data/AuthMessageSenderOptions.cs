5eaf6e3e6ba430883dbaad9aded8727d18f64f2c
andreicruceru95 <56835442+andreicruceru95@users.noreply.github.com>
2021-10-10 18:33:21 +0100

    Add project files.

---
+++ StoreApp/Areas/Identity/Data/AuthMessageSenderOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreApp.Areas.Identity.Data
+{
+    public class AuthMessageSenderOptions
+    {
+        public readonly string SendGridKey = "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";
+    }
+}
---
+++ StoreApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StoreApp.Areas.Identity.Data;
+using StoreApp.Data;
+
+[assembly: HostingStartup(typeof(StoreApp.Areas.Identity.IdentityHostingStartup))]
+namespace StoreApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<StoreAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("StoreAppContextConnection")));
+
+                services.AddDefaultIdentity<StoreAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<StoreAppContext>();
+            });
+        }
+    }
+}
